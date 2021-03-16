0ab63a06644fb8c8f791c21ed416b140eb76fbcc
shi <shi@crtweb.ru>
2021-03-16 12:30:34 +0500

    fix and refactor

--- todo.js
+++ todo.js
@@ -13,13 +13,9 @@ function TodoList() {
     addTask({ description: 'use rest api backend', completed: false});
 
     return {
-        complete_task: function (task_id) {
+        change_complete_task: function (task_id, completed) {
             task = tasks.get(parseInt(task_id));
-            task.completed = true;
-        },
-        uncomplete_task : function(task_id) {
-            task = tasks.get(parseInt(task_id));
-            task.completed = false;
+            task.completed = completed;
         },
         edit_task: function(task_id, description) {
             task = tasks.get(parseInt(task_id));
@@ -29,7 +25,7 @@ function TodoList() {
             return tasks.get(task_id);
         },
         add_task: function (task) {
-            return addTask(task);
+            addTask(task);
         },
         remove_task: function (task_id) {
             return tasks.delete(parseInt(task_id));
@@ -47,19 +43,27 @@ function TodoList() {
 var todoList = new TodoList();
 
 function add() {
-    var description = document.getElementById('task').value;
+    var $input_task = $('#task');
+    var description = $input_task.val();
+
+    if (description.trim() === '') return false;
 
     todoList.add_task({'description': description, 'completed': false});
     showTaskList();
+    $input_task.val('');
     return true;
 }
 
 function changeLabel() {
-    var id = this.getAttribute('id'),
-        description = $('label[id="' + id + '"]').val();
+    var id = this.getAttribute('id');
+    var $label = $('label[id="' + id + '"]');
+    var $input = $('.edit-input[id="' + id + '"]');
 
-    $('label[id="' + id + '"]').hide();
-    $('.edit-input[id="' + id + '"]').show().focus();
+    $label.hide();
+    $input.show().focus();
+
+    //вот без этого была ошибка. При тыке на label - теперь передаваем в input старое зачение для редактирования, без этого было пусто.
+    $input.val($label.text());
     return false;
 }
 
@@ -72,14 +76,10 @@ function labelChanged() {
     return false;
 }
 
-function changeStatus(task_id) {
-    var task = todoList.get_task(task_id);
-
-    if (task.completed == true) {
-        todoList.uncomplete_task(task_id)
-    } else {
-        todoList.complete_task(task_id)
-    }
+function changeStatus() {
+    var task_id = this.getAttribute('id');
+    var task = todoList.get_task(parseInt(task_id));
+    todoList.change_complete_task(task_id, !task.completed)
     showTaskList();
 }
 
@@ -107,17 +107,16 @@ function remove() {
 var taskList = function() {
     var html = '';
     for (var todo of todoList.all_tasks()) {
-        var onclick ="onClick=\"changeStatus(" + todo.id + ")\"",
-            checked = checkedProperty(todo.id);
-
-        html += '<div class="input-group style"><span class="input-group"><input type="checkbox" id="' + todo.id + '" ' + onclick + ' ' + checked + '><label for="checkbox" id="' + todo.id + '" class="edit">' + todo.description + '</label><input class="edit-input" id="' + todo.id + '"/></span><span class="input-group-btn"><button aria-label="Close" class="close remove" id="' + todo.id + '"><span aria-hidden="true">&times;</span></button></span></div>';
+        var checked = checkedProperty(todo.id);
+        html += '<div class="input-group style"><span class="input-group"><input type="checkbox" id="' + todo.id + '" ' + checked + '><label for="checkbox" id="' + todo.id + '" class="edit">' + todo.description + '</label><input class="edit-input" id="' + todo.id + '"/></span><span class="input-group-btn"><button aria-label="Close" class="close remove" id="' + todo.id + '"><span aria-hidden="true">&times;</span></button></span></div>';
     }
 
     document.getElementById('todos').innerHTML = html;
 
     var buttons = document.getElementsByClassName('remove'),
         edit = document.getElementsByClassName('edit'),
-        edit_inputs = document.getElementsByClassName('edit-input');
+        edit_inputs = document.getElementsByClassName('edit-input'),
+        checkboxes = $("input[type=checkbox]");
 
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', remove);
@@ -130,6 +129,10 @@ var taskList = function() {
 
     for (var i = 0; i < edit_inputs.length; i++) {
         edit_inputs[i].addEventListener('focusout', labelChanged);
+    };
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('click', changeStatus);
     };
 }
 
@@ -144,84 +147,52 @@ function calculateCounter() {
 
 var activeFilter = "all";
 
-function filterByAll() {
-    activeFilter = "all";
-    var inputs = $("input[type=checkbox]");
-    $('#allFilter').addClass("active");
-    $('#completedFilter').removeClass("active");
-    $('#activeFilter').removeClass("active");
-    return inputs.parents().show();
-}
-document.querySelector("#allFilter").addEventListener('click', filterByAll, false);
-
-function filterByActive() {
-    activeFilter = "active";
+//из трех - один метод и передаю тип в листенере
+function filterTasks(filter) {
+    activeFilter = filter;
     var $inputs = $("div input[type=checkbox]"),
-        $inputsCh = $inputs.filter(":checked"),
-        $inputsNotCh = $inputs.filter(":not(:checked)"),
-        $parentInputs = $inputsCh.parent();
-    $('#activeFilter').addClass("active");
-    $('#allFilter').removeClass("active");
-    $('#completedFilter').removeClass("active");
-    return ($parentInputs.hide(), $inputsNotCh.parents().show());
-    showTaskList();
-}
-document.querySelector("#activeFilter").addEventListener('click', filterByActive, false);
-
-function filterByCompleted() {
-    activeFilter = "completed";
-    var $inputs = $("div input[type=checkbox]"),
-        $inputsCh = $inputs.filter(":checked"),
-        $inputsNotCh = $inputs.filter(":not(:checked)"),
-        $parentInputs = $inputsNotCh.parent();
-    $('#completedFilter').addClass("active");
-    $('#allFilter').removeClass("active");
-    $('#activeFilter').removeClass("active");
-    return ($parentInputs.hide(), $inputsCh.parents().show());
-    showTaskList();
-}
-document.querySelector("#completedFilter").addEventListener('click', filterByCompleted, false);
-
-var filterTasksBy = function(filter) {
-    if(activeFilter == "all") {
-        filterByAll();
-    } else if(activeFilter == "active") {
-        filterByActive();
-    } else {
-        filterByCompleted();
+        $inputsCh = $inputs.filter(":checked").parent().parent(),
+        $inputsNotCh = $inputs.filter(":not(:checked)").parent().parent();
+    $('#filterGroup>p').removeClass("active");
+    switch(activeFilter) {
+        case 'active':
+            $('#activeFilter').addClass("active");
+            return ($inputsCh.hide(), $inputsNotCh.show());
+        case 'completed':
+            $('#completedFilter').addClass("active");
+            return ($inputsNotCh.hide(), $inputsCh.show());
+        default:
+            $('#allFilter').addClass("active");
+            return $inputs.parent().parent().show();
     }
+    //showTaskList() - в конце не нужен уже был. и не достижимый кусок
 }
+
+document.querySelector("#allFilter").addEventListener('click', function () {filterTasks('all')}, false);
+document.querySelector("#activeFilter").addEventListener('click', function () {filterTasks('active')}, false);
+document.querySelector("#completedFilter").addEventListener('click', function () {filterTasks('completed')}, false);
 
 function showTaskList() {
     taskList();
-    filterTasksBy(activeFilter);
+    filterTasks(activeFilter);
     calculateCounter();
 }
 
 document.getElementById('add').addEventListener('click', add);
 showTaskList();
 
-function selectAll() {
+//из двух - один метод Выделить все/снять выделение
+function selectChange(checked) {
     var inputs = $("input[type=checkbox]");
 
     for (var i = 0; i < inputs.length; i++ ) {
         inputs[i].checked=true;
-        todoList.complete_task(inputs[i].id);
-        showTaskList();
-    }
-}
-document.querySelector("#selectAll").addEventListener('click', selectAll, false);
-
-function deselectAll() {
-    var inputs = $("input[type=checkbox]");
-
-    for (var i = 0; i < inputs.length; i++ ) {
-        inputs[i].checked=false;
-        todoList.uncomplete_task(inputs[i].id);
+        todoList.change_complete_task(inputs[i].id, checked);
     }
     showTaskList();
 }
-document.querySelector("#deselectAll").addEventListener('click', deselectAll, false);
+document.querySelector("#selectAll").addEventListener('click', function (){selectChange(true)}, false);
+document.querySelector("#deselectAll").addEventListener('click', function (){selectChange(false)}, false);
 
 function completedRemove() {
     var inputs = $("input[type=checkbox]");
@@ -234,3 +205,7 @@ function completedRemove() {
     }
 }
 document.querySelector("#completedRemove").addEventListener('click', completedRemove, false);
+
+//вот без этого была ошибка. Не нужно отправлять ничего на сервер и перегружать форму.
+function handleSubmit(event) {event.preventDefault();}
+document.querySelector("#main").addEventListener('submit', handleSubmit, false);
